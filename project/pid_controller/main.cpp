bff558a6d00b615e13f3b496d0b1af663995996e
Hsin-Wen Chang <bow1226@gmail.com>
2022-04-26 13:39:56 +0800

    fix: Velocity, steer and throttle sent are all 0

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -219,14 +219,14 @@ int main ()
   * (Step 1): create pid (pid_steer) for steer command and initialize values
   **/
   PID pid_steer = PID();
-  pid_steer.Init(0.2, 0.0, 0.0, 1, -1);
+  pid_steer.Init(0.3, 0.1, 0.2, 1.2, -1.2);
 
   // initialize pid throttle
   /**
   * (Step 1): create pid (pid_throttle) for throttle command and initialize values
   **/
   PID pid_throttle = PID();
-  pid_throttle.Init(0.02, 0.0, 0.0, 1, -1);
+  pid_throttle.Init(0.3, 0.2, 0.2, 1, -1);
   
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
